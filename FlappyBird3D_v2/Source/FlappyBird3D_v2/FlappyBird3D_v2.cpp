65345af0501664de09e321a93f12208d67a2af94
Feureau-PC <feureau@gmail.com>
2018-01-29 14:36:43 +0700

    Removed v2

--- FlappyBird3D_v2/Source/FlappyBird3D_v2/FlappyBird3D_v2.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "FlappyBird3D_v2.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FlappyBird3D_v2, "FlappyBird3D_v2" );
